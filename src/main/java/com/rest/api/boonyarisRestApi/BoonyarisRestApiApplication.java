09e72c8ef64c6d67cdcee8f7a4dd2a5e7dbfd2d1
Boonyaris Pensri <boonyaris.p@aware.co.th>
2023-12-26 14:40:44 +0700

    first commit

---
+++ src/main/java/com/rest/api/boonyarisRestApi/BoonyarisRestApiApplication.java
@@ -0,0 +1,14 @@
+package com.rest.api.boonyarisRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoonyarisRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoonyarisRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rest/api/boonyarisRestApi/BoonyarisRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.api.boonyarisRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoonyarisRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
